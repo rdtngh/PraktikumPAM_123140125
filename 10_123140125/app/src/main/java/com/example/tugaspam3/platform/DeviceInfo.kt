8f15640069d337372ca132efe15852e21679e876
rdtngh <raditya.123140125@student.itera.ac.id>
2026-05-08 17:29:56 +0700

    perbaikan

--- 10_123140125/app/src/main/java/com/example/tugaspam3/platform/DeviceInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tugaspam3.platform
-
-interface DeviceInfo {
-    val model: String
-    val manufacturer: String
-    val osVersion: String
-    val apiLevel: Int
-}
--- 10_123140125/app/src/main/java/com/example/tugaspam3/platform/NetworkMonitor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tugaspam3.platform
-
-import kotlinx.coroutines.flow.Flow
-
-interface NetworkMonitor {
-    val isConnected: Flow<Boolean>
-}
